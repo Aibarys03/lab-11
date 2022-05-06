b09e2020407a269a55cdade9eca58adee1fd2559
Aibarys03 <aibarys0490@gmail.com>
2022-05-06 08:13:42 +0600

    Updates

---
+++ images/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+import Header from './components/Header';
+import Main from './components/Main';
+import Footer from './components/Footer';
+
+
+
+function App() {
+  return (
+    <div className="App">
+     <Header/>
+      <Main/>
+      <Footer address="Auezov st. 79 , Almaty city"/>
+    </div>
+  );
+}
+
+export default App;
---
+++ images/components/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Header(){
+    return(
+    <div className="Header">
+        <header className="App-header">
+        <div className='text'>
+English Language Tutors</div>
+<div className='logo'>
+<img className='logol' src="../images/Tutor.png" alt="logo"/>
+</div>
+        </header>
+    </div>
+    );
+}
+
+export default Header;
